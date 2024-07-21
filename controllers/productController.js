6451152cdf0f136fa9a26147701d2128c1b5af77
AomThippawan <aomthippawan1121@gmail.com>
2024-07-21 20:13:17 +0700

    first commit

---
+++ controllers/productController.js
@@ -0,0 +1,63 @@
+const Product = require('../models/product'); // Import the Product model
+
+// Get all products
+exports.getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Get a single product by ID
+exports.getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        res.json(product);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+// Create a new product
+exports.createProduct = async (req, res) => {
+    const { product_name, product_type, price, unit } = req.body;
+    const product = new Product({ product_name, product_type, price, unit });
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Update a product by ID
+exports.updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        const updates = req.body;
+        Object.assign(product, updates);
+        const updatedProduct = await product.save();
+        res.json(updatedProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// Delete a product by ID
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) return res.status(404).json({ message: 'Product not found' });
+        await Product.findByIdAndDelete(id);
+        res.json({ message: 'Product deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
